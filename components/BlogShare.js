17e66a25fbb1e782b6832bdbef15c3276e89dd60
Thanga <thangamariappancse@gmail.com>
2022-01-28 12:22:22 +0530

    fixes

--- components/BlogShare.js
+++ components/BlogShare.js
@@ -3,12 +3,12 @@ import { FaTwitter } from "react-icons/fa";
 function BlogShare({ data }) {
   return (
     <>
-      <div className="text-center pb-4">
+      {/* <div className="text-center pb-4">
         <button className="bg-indigo-500 px-3 py-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
           <FaTwitter />
           
         </button>
-      </div>
+      </div> */}
     </>
   );
 }
